9301b2705bf48884debdbf3a39c684e491cebade
daniel ouattara <daniel.ouattara@gmx.com>
2021-01-18 10:41:53 +0100

    app. comme dans gofullstack nmini-project

--- serveur/app.js
+++ serveur/app.js
@@ -2,27 +2,30 @@ const express = require ('express');  // importe 'express'
 
 const app = express(); //  cree une application express
 
+app.use('/api/stuff', (req, res, next) => {
 
-app.use((req, res, next) => {
-    console.log("Requête reçue");
-    next();
-});
+    const stuff = [
+        {
+          _id: 'oeihfzeoi',
+          title: 'Mon premier objet',
+          description: 'Les infos de mon premier objet',
+          imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+          price: 4900,
+          userId: 'qsomihvqios',
+        },
+        {
+          _id: 'oeihfzeomoihi',
+          title: 'Mon deuxième objet',
+          description: 'Les infos de mon deuxième objet',
+          imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+          price: 2900,
+          userId: 'qsomihvqios',
+        },
+      ];
 
+    res.status(200).json(stuff);
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-
-app.use((req, res, next) => {  // configuration réponse simple
-    res.json({ message: 'Requête traîtée !'});
-    next();
-});
-
-
-app.use((req, res) => {  // configuration réponse simple
-    console.log('Réponse envoyée avec succès');
 });
 
 module.exports = app;  //  rend 'app' accessible depuis les autres fichiers du projet
+
