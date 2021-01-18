76d927c44ef5f6cfc620540b591068d3e1c624c6
daniel ouattara <daniel.ouattara@gmx.com>
2021-01-18 15:15:02 +0100

    création des routes spécifiques

--- serveur/app.js
+++ serveur/app.js
@@ -5,7 +5,7 @@ const app = express(); //  cree une application express
 
 const mongoose = require('mogoose');
 
-const SchemaUtilisateurs = require('./schemaDeDonnees/SchemaUtilisateur.js')
+const SchemaUtilisateur = require('./schemaDeDonnees/SchemaUtilisateur.js')
 const SchemaSauce = require('./schemaDeDonnees/SchemaSauce.js')
 
 mongoose.connect('mongodb+srv://danielOuattaraSepekocko:Zl8UkVDuAUXJ9MvEZl8UkVDuAUXJ9MvE@cluster0.vndw3.mongodb.net/test?retryWrites=true&w=majority',
@@ -26,59 +26,86 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/auth/signup', (req, res, next) => {   // recupère les inscriptions
-    console.log(req.body);
-    res.status(201).json({
-        message: 'inscription réussie'
-    });
+
+    if (req.body._id) delete req.body._id;
+    const schemaUtilisateur = new SchemaUtilisateur({ ... req.body});
+    schemaUtilisateur.save()
+        .then( () => res.status(201).json({ message: 'Inscription Réussie. Bienvenue Chez Se Pokocko !'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
 });
 
 app.post('/api/auth/login', (req, res, next) => {   // récupère les connexions
-    console.log(req.body);
-    res.status(201).json({
-        message: 'connexion reussie'
-    });
+    const schemaUtilisateur = new SchemaUtilisateur({ ... req.body});
+    schemaUtilisateur.save()
+        .then( () => res.status(201).json({ message: 'Connexion Réussie, Content De Vous Revoir'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
+
 });
 
 app.post('/api/sauces', (req, res, next) => {   // recupère les sauces
-    console.log(req.body);
-    res.status(201).json({
-        message: 'sauce captée'
-    });
+    const schemaSauce = new SchemaSauce({ ... req.body});
+    schemaSauce.save()
+        .then( () => res.status(201).json({ message: ' Réussie'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
 });
 
 app.post('/api/sauces/:id/like', (req, res, next) => {   // écouter les requêtes formulaires
-    console.log(req.body);
-    res.status(201).json({
-        message: 'sauce aimée (ou pas ?..ici ?)'
-    });
+    const schemaSauce = new SchemaSauce({ ... req.body});
+    schemaSauce.save()
+        .then( () => res.status(201).json({ message: ' Message Enregisttré Réussie'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
 });
 
 app.get('/api/sauces', (req, res, next) => {   // renvoie tableau de toutes les sauces au client de la BD
-    console.log(req.body);
-    res.status(201).json({
-        message: 'voici les sauces'
-    });
+    const schemaSauce = new SchemaSauce({ ... req.body});
+    schemaSauce.save()
+        .then( () => res.status(201).json({ message: ' Affichage Tableau De Sauce Réussie'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
 });
 
 app.get('/api/sauces/:id', (req, res, next) => {   // renvoie la sauce spécifique avec son ID au client
-    console.log(req.body);
-    res.status(201).json({
-        message: 'voici les sauces'
-    });
+    const schemaSauce = new SchemaSauce({ ... req.body});
+    schemaSauce.save()
+        .then( () => res.status(201).json({ message: ' Renvoi Sauce Spécifique Réussie'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
 });
 
-app.put('/api/sauces/:id', (req, res, next) => {   // renvoie la sauce spécifique avec son ID au client
-    console.log(req.body);
-    res.status(201).json({
-        message: 'voici les sauces'
-    });
+app.put('/api/sauces/:id', (req, res, next) => {   // actualise la sauce spécifique avec son ID au client
+    const schemaSauce = new SchemaSauce({ ... req.body});
+    schemaSauce.save()
+        .then( () => res.status(201).json({ message: ' Réussie'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
 });
 
 app.delete('/api/sauces/:id', (req, res, next) => {   // supprime la sauce spécifique avec son ID au client
-    console.log(req.body);
-    res.status(201).json({
-        message: 'voici les sauces'
-    });
+    const schemaSauce = new SchemaSauce({ ... req.body});
+    schemaSauce.save()
+        .then( () => res.status(201).json({ message: ' Réussie'}))
+        .catch( error => {
+            console.log(error);
+            res.status(400).json({error});
+        })
 });
 
 
