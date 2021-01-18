435ed9b42de66fa4d4f3f434245bfae5ba44dc4d
daniel ouattara <daniel.ouattara@gmx.com>
2021-01-18 10:40:06 +0100

    app.js + middleware

--- serveur/app.js
+++ serveur/app.js
@@ -2,11 +2,27 @@ const express = require ('express');  // importe 'express'
 
 const app = express(); //  cree une application express
 
+
+app.use((req, res, next) => {
+    console.log("Requête reçue");
+    next();
+});
+
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+
+app.use((req, res, next) => {  // configuration réponse simple
+    res.json({ message: 'Requête traîtée !'});
+    next();
+});
+
+
 app.use((req, res) => {  // configuration réponse simple
-    res.json( { 
-        message_1: 'Requête 1 reçue !', 
-        message_2: 'Requête 2 reçue !'
-    });
+    console.log('Réponse envoyée avec succès');
 });
 
 module.exports = app;  //  rend 'app' accessible depuis les autres fichiers du projet
