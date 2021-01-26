b934d5e1fa7825c3caff6703a1569cdbacc30792
daniel ouattara <daniel.ouattara@gmx.com>
2021-01-26 16:22:26 +0100

    endpointProtection( )

--- backend/routes/sauceRoutes.js
+++ backend/routes/sauceRoutes.js
@@ -1,8 +1,8 @@
 const express          = require('express');
 const router           = express.Router();
-const auth             = require('../auth/auth.js');
-const saucesController = require('../controllers/sauceControllers.js');
-const multer           = require('../auth/multer-config.js');
+const auth             = require('./../auth/auth.js');
+const saucesController = require('./../controllers/sauceControllers.js');
+const multer           = require('./../auth/multer-config.js');
 
 
 router.post('/',         auth, multer, saucesController.addSauce );
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express  = require('express');
 const router   = express.Router();
 
-const  usersController = require('../controllers/userControllers.js')
+const  usersController   = require('../controllers/userControllers.js')
+const endpointProtection = require('./../auth/endpointProtection.js')
 
-
-router.post('/signup', usersController.singup);
-router.post('/login',  usersController.login);
+router.post('/signup', endpointProtection, usersController.singup);
+router.post('/login',  endpointProtection, usersController.login);
 
 module.exports = router; 
