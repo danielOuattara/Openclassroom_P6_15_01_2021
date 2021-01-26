59cc083744d5ea9f9c7732a5fa58b65329d0522b
daniel ouattara <daniel.ouattara@gmx.com>
2021-01-26 18:12:43 +0100

    mot de pass : => strong required  !

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -7,7 +7,7 @@ const validator    = require('email-validator');
 exports.singup = (req, res, next) => {  
 
     if (!validator.validate(req.body.email)) {
-        return res.status(401).json({error} )              // 'Email Not Valid'
+        return res.status(401).json({error: 'This email address is not valid'} )              // 'Email Not Valid'
     }
 
     User.findOne( {email: req.body.email} )
@@ -16,6 +16,10 @@ exports.singup = (req, res, next) => {
         if(userCheck) {
             return res.status(401).json( {error: 'This email address is already used !'}) 
         }
+
+        if (!/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!?&#@$%µ€_])[a-zA-Z0-9!?&#@$%µ€]{7,}/.test(req.body.password)) {   // Test password strength
+            return res.status(401).json({ error: 'Password is a minimum of 7 characters, at least: of 1 lower case letter, 1 uppercase letter, 1 digit, 1 specila between _ ! ? & # @ $ % µ € ' });
+          } 
        
         bcrypt.hash( req.body.password, 13)
         .then( hash => {
@@ -35,33 +39,8 @@ exports.singup = (req, res, next) => {
 }
 
 
- /*Back Up
- =============*/
-// exports.singup = (req, res, next) => {   
-
-//     bcrypt.hash( req.body.password, 10)
-//     .then( hash => {
-//         const user = new User( 
-//             {
-//                 email: req.body.email,
-//                 password: hash
-//             }
-//         );
-//         user.save()
-//         .then( () => res.status(201).json( {message: 'User Created !'}))
-//         .catch( error =>  {
-//             console.log(error); 
-//             res.status(400).json( {error})
-//         })
-//     })
-//     .catch(error => res.status(500).json( {error}))
-// }
-
-
 
 exports.login = (req, res, next) => {  
-    // console.table(req.body);
-    // console.log(req.body); 
    
     if (!validator.validate(req.body.email)) {
         return res.status(401).json({error} )              // 'Email Not Valid'
@@ -74,15 +53,16 @@ exports.login = (req, res, next) => {
         }
         bcrypt.compare( req.body.password, user.password)
         .then( valid => {
-            if( !valid) {
+            if(valid) {
+                res.status(200).json( 
+                    { 
+                       userId: user._id,
+                       token: jsonwebtoken.sign( { userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '1h' } )
+                    }
+                )
+            } else {          
                 return res.status(401).json( {error} )    // Password Not Recognized
             }
-            res.status(200).json( 
-                { 
-                   userId: user._id,
-                   token: jsonwebtoken.sign( { userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '1h' } )
-                }
-            )
         })
         .catch( error => res.status(500).json( {error} ))
     })
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,9 @@ const express  = require('express');
 const router   = express.Router();
 
 const  usersController   = require('../controllers/userControllers.js')
-const endpointProtection = require('./../auth/endpointProtection.js')
+// const endpointProtection = require('./../auth/endpointProtection.js')
 
-router.post('/signup', endpointProtection, usersController.singup);
-router.post('/login',  endpointProtection, usersController.login);
+router.post('/signup', /*  endpointProtection, */ usersController.singup);
+router.post('/login', /*  endpointProtection */ usersController.login);
 
 module.exports = router; 
